0e4ba31f0d5e321880cc54fa47ed344fe621cfd2
Vincent Durmont <vincent@lightspark.com>
2025-02-23 21:25:56 -0800

    Fetch wallets with SDK

--- www/src/app/wallets/page.tsx
+++ www/src/app/wallets/page.tsx
@@ -1,8 +1,35 @@
 "use client";
 
+import { SparkWallet } from "@buildonspark/spark-js-sdk";
+import { Network } from "@buildonspark/spark-js-sdk/utils";
+import { useEffect, useState } from "react";
+
+type Wallet = {
+  sparkWallet: SparkWallet;
+  balance: BigInt;
+  pubkey: string;
+};
+
 export default function Wallets() {
-  const rawMnemonics = localStorage.getItem("SATOSHIS_APP_MNEMONICS");
-  const mnemonics = rawMnemonics ? JSON.parse(rawMnemonics) : [];
+  const [wallets, setWallets] = useState<Array<Wallet>>([]);
+
+  useEffect(() => {
+    const rawMnemonics = localStorage.getItem("SATOSHIS_APP_MNEMONICS");
+    const mnemonics = rawMnemonics ? JSON.parse(rawMnemonics) : [];
+    for (const mnemonic of mnemonics) {
+      const wallet = new SparkWallet(Network.REGTEST);
+      wallet.createSparkWallet(mnemonic).then(() => {
+        wallet.getBalance().then((balance) => {
+          wallet.getMasterPubKey().then((masterPubKey) => {
+            const pubkey = Buffer.from(masterPubKey).toString("hex");
+            setWallets(
+              wallets.concat([{ sparkWallet: wallet, balance, pubkey }])
+            );
+          });
+        });
+      });
+    }
+  }, [setWallets]);
 
   return (
     <div className="flex flex-col items-center justify-start min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -11,10 +38,19 @@ export default function Wallets() {
       </h1>
       <div className="flex flex-col sm:flex-row gap-4 mt-10">
         <p className="mb-5">
-          You have {mnemonics.length} wallet{mnemonics.length > 1 ? "s" : ""}.
+          You have {wallets.length} wallet{wallets.length > 1 ? "s" : ""}.
         </p>
+        {wallets.map((wallet, index) => (
+          <div
+            key={index}
+            className="flex flex-col gap-2  border-solid border-color-primary border-2 p-4 rounded-lg"
+          >
+            <p>{wallet.pubkey.substring(0, 20)}...</p>
+            <p>Balance: {String(wallet.balance)} sats</p>
+          </div>
+        ))}
         <a
-          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          className="rounded-full mt-5 border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
           href="/wallets/new"
         >
           Create a wallet
